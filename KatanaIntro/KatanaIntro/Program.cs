9117c9db82dba99a03b541f40a0a0338b50241eb
Christopher Catilo <chriscatilo@hotmail.com>
2016-06-07 17:06:40 +0100

    Katana Hello World

--- KatanaIntro/KatanaIntro/Program.cs
+++ KatanaIntro/KatanaIntro/Program.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
 
 namespace KatanaIntro
 {
@@ -10,6 +8,26 @@ namespace KatanaIntro
     {
         static void Main(string[] args)
         {
+            // start a web server listening to a given http address
+            var uri = "http://localhost:8080";
+            using (WebApp.Start<Startup>(uri))
+            {
+                Console.WriteLine("Started!");
+                Console.ReadKey();
+                Console.WriteLine("Stopping");
+            }
         }
     }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(ctx =>
+            {
+                return ctx.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+
 }
